b040e1a342f7673e74d0dbc755ced4cbb4fd7206
Santpapen <lotopara@gmail.com>
2019-01-18 14:24:07 +0100

    English text

--- main.cs
+++ main.cs
@@ -1,12 +1,14 @@
 /*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * 
 Author: Roland Santiago
-Fecha: 15/01/2018
-Proyecto_base: Apt_windows
-Descripción: 
+Date: 15/01/2018
+Project_base: Apt_windows
+Description: First version (Download pool files of the Debian repository)
  */
 using System;
 using System.IO;
- using System.IO.Compression;
+using System.IO.Compression;
 using System.Net;
 
 
@@ -14,15 +16,17 @@ public class Apt_windows
 {
     public static void Main()
     {
+        Global data = new Global();
         Apt_windows apt = new Apt_windows();       
-        apt.md5sumsDownload ();
-        apt.readFile("md5sums");
-        Console.ReadKey(); // Para el Cmd y se queda a la espera
+        apt.md5sumsDownload (); //Download Index File
+        apt.readFile("md5sums"); 
+        Console.ReadKey(); // Stopping Cmd and wait for input keyboard
     } // End Main
+
     public void md5sumsDownload(){
-        string url = "http://ftp.es.debian.org/debian/indices/";
+        string url = "http://ftp.es.debian.org/debian/indices/"; // index File path
         string filename = "md5sums.gz";
-        Console.WriteLine ("Descarga de archivo indices");
+        Console.WriteLine ("Download index File,Please wait ...");
         FileDownload (url,filename);
         string directoryPath = @".";
         DirectoryInfo directorySelect = new DirectoryInfo(directoryPath);
@@ -32,24 +36,25 @@ public class Apt_windows
         }
     }
     public void FileDownload(string uri,string file){
-        //string remoteUri ="https://www.alemansencillo.com/img/ns/";
-        //string fileName = "logo.gif";
+        Global proxyData = new Global();
         string myStringWebResource = null;
 
         // create Webclient instance
         using (WebClient myWebClient = new WebClient()){
-            
-                myStringWebResource = uri + file;
+        
+                  myStringWebResource = uri + file;
+
                 // System Logon Credentials
                 myWebClient.UseDefaultCredentials = true;
                 myWebClient.Credentials = CredentialCache.DefaultCredentials;
+
             try
             {
                 // Download File
                 myWebClient.DownloadFile(myStringWebResource, file);
             }catch (WebException webEx)
             {
-                Console.WriteLine(webEx.Message);
+                Console.WriteLine("Error Http: {0}",webEx.Message);
                 
             } // EndTryCatch
             
@@ -75,11 +80,12 @@ public class Apt_windows
 
     public void readFile(string File)
         {
-        Console.WriteLine("Lectura");
-        /* Lectura del Fichero Package */
+        Console.WriteLine("Starting files download,please wait ... \n You can have a cup of coffee or tea (Press intro);-)");
+        Console.ReadKey();
+        /* Read File Packages Directory */
         string[] dataPackage = new string[3];
-        // bool printData = false;
-        /*Funcion de Parseo y Lectura de Packages_debian */
+      
+        /*Function parse and read Packages_debian */
         try
         {
             using (StreamReader sr = new StreamReader("md5sums"))
@@ -88,25 +94,23 @@ public class Apt_windows
                 string downloadDir = null;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    bool aValue = line.Contains("pool/");
+                    bool aValue = line.Contains("pool/"); // Filter pool directory
                     if (aValue)
                     {
-                        // Console.WriteLine(line);
-                        string[] items = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        string[] items = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); // Parse doble space
                         int LastMax = items[1].Split('/').Length;
                         string[] Filename = items[1].Split('/');
                         for (int i = 0; i < LastMax - 1; i++)
                         {
-                            downloadDir = downloadDir + Filename[i] + "/";
+                            downloadDir = downloadDir + Filename[i] + "/"; // build local path directory
                         }
                         string dbFilename = Filename[LastMax - 1];
-                        //Console.WriteLine(downloadDir);
-
-                        if (!System.IO.Directory.Exists(downloadDir))
+           
+                        if (!System.IO.Directory.Exists(downloadDir)) 
                         {
                             try
                             {
-                                System.IO.Directory.CreateDirectory(downloadDir);
+                                System.IO.Directory.CreateDirectory(downloadDir); // Create directory if !Exist
                             }
                             catch
                             {
@@ -114,22 +118,22 @@ public class Apt_windows
                             }
 
                         }
+                        // Download Files
                         string url = "http://ftp.es.debian.org/debian/";
                         string filename = items[1];
                         FileDownload(url, filename);
-                        Console.WriteLine(dbFilename);
+                        Console.WriteLine(dbFilename); // Output of Files Downloader
                         downloadDir = null;
                     } //pool Filter
 
                 } // While ReadLine
   
-            } // Apertura de Fichero
+            } // Open md5sums
 
         }
         catch (Exception e)
         {
-            Console.WriteLine("No es posible abrir el Fichero");
-            Console.WriteLine(e.Message);
+            Console.WriteLine("Error Open File: {0}", e.Message);
         }
     } // End readFile
 } // End Class
